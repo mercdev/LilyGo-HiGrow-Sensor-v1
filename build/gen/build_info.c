1de97a9bbe076ab732fde9b398646a7f0983b776
Jason Harrell <jason-harrell@iMac-Pro.lan>
2018-04-24 14:14:09 -0600

    Removed /build
    Removed /build folder

--- build/gen/build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *build_id = "20180218-182818/???";
-const char *build_timestamp = "2018-02-18T18:28:18Z";
-const char *build_version = "1.0";
--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20180218-182818/???";
-const char *mg_build_timestamp = "2018-02-18T18:28:18Z";
-const char *mg_build_version = "2018021818";
